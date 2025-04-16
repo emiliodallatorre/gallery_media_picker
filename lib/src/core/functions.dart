e8276dddbcd4a2436b5ed977c474281245e9aa64
matteogennaersaf <m.genna@ersaf.it>
2025-04-16 16:41:50 +0200

    debugging aggiuntivo

--- lib/src/core/functions.dart
+++ lib/src/core/functions.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer' as dev;
 
 import 'package:flutter/material.dart';
 import 'package:gallery_media_picker/src/presentation/pages/gallery_media_picker_controller.dart';
@@ -39,7 +40,11 @@ class GalleryFunctions {
         builder: (context) => GestureDetector(
               behavior: HitTestBehavior.translucent,
               onTap: () => close(null),
-              child: OverlayDropDown(height: height!, close: close, animationController: animationController, builder: builder),
+              child: OverlayDropDown(
+                  height: height!,
+                  close: close,
+                  animationController: animationController,
+                  builder: builder),
             ));
     Overlay.of(context).insert(entry);
     animationController.animateTo(1);
@@ -50,43 +55,86 @@ class GalleryFunctions {
   }
 
   static onPickMax(GalleryMediaPickerController provider) {
-    provider.onPickMax.addListener(() => showToast("Already pick ${provider.max} items."));
+    provider.onPickMax.addListener(() {
+      dev.log('Maximum items picked: ${provider.max}',
+          name: 'GalleryMediaPicker');
+      showToast("Already pick ${provider.max} items.");
+    });
   }
 
-  static getPermission(setState, GalleryMediaPickerController provider, final RequestType requestType) async {
+  static getPermission(setState, GalleryMediaPickerController provider,
+      final RequestType requestType) async {
     /// request for device permission
-    var result = await PhotoManager.requestPermissionExtend(requestOption: const PermissionRequestOption(iosAccessLevel: IosAccessLevel.readWrite));
+    dev.log(
+        'Requesting media permission with access level: ${IosAccessLevel.readWrite}',
+        name: 'GalleryMediaPicker');
+    var result = await PhotoManager.requestPermissionExtend(
+        requestOption: const PermissionRequestOption(
+            iosAccessLevel: IosAccessLevel.readWrite));
     if (result.isAuth) {
       /// load "recent" album
+      dev.log('Permission granted successfully', name: 'GalleryMediaPicker');
       provider.setAssetCount();
       PhotoManager.startChangeNotify();
+      dev.log('Started change notification listener',
+          name: 'GalleryMediaPicker');
       PhotoManager.addChangeCallback((value) {
+        dev.log('Media change detected, refreshing path list',
+            name: 'GalleryMediaPicker');
         _refreshPathList(setState, provider, requestType);
       });
 
       if (provider.pathList.isEmpty) {
+        dev.log('Path list is empty, performing initial refresh',
+            name: 'GalleryMediaPicker');
         _refreshPathList(setState, provider, requestType);
       }
     } else {
       /// if result is fail, you can call `PhotoManager.openSetting();`
       /// to open android/ios application's setting to get permission
+      dev.log('Permission denied. Auth state: ${result.toString()}',
+          name: 'GalleryMediaPicker');
       PhotoManager.openSetting();
     }
   }
 
-  static _refreshPathList(setState, GalleryMediaPickerController provider, final RequestType requestType) {
+  static _refreshPathList(setState, GalleryMediaPickerController provider,
+      final RequestType requestType) {
+    dev.log('Refreshing path list for media type: ${requestType.toString()}',
+        name: 'GalleryMediaPicker');
     PhotoManager.getAssetPathList(type: requestType).then((pathList) {
       /// don't delete setState
+      dev.log('Retrieved ${pathList.length} media paths',
+          name: 'GalleryMediaPicker');
       setState(() {
         provider.resetPathList(pathList);
+        dev.log('Path list updated in provider', name: 'GalleryMediaPicker');
       });
+    }).catchError((error) {
+      dev.log('Error refreshing path list: $error',
+          name: 'GalleryMediaPicker', error: error);
     });
   }
 
   /// get asset path
   static Future getFile(AssetEntity asset) async {
-    var file = await asset.file;
-    return file!.path;
+    dev.log('Getting file for asset: ${asset.id}', name: 'GalleryMediaPicker');
+    try {
+      var file = await asset.file;
+      if (file != null) {
+        dev.log('Successfully retrieved file: ${file.path}',
+            name: 'GalleryMediaPicker');
+        return file.path;
+      } else {
+        dev.log('Retrieved null file for asset: ${asset.id}',
+            name: 'GalleryMediaPicker');
+        return null;
+      }
+    } catch (e) {
+      dev.log('Error getting file for asset ${asset.id}: $e',
+          name: 'GalleryMediaPicker', error: e);
+      return null;
+    }
   }
 }
 
